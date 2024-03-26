22f12f4447ebdc645f66c04e91880612aa497b47
alsth712 <alsth712@naver.com>
2024-03-26 09:49:47 +0900

    회원 포인트 누적 및 등급 산정 기능 추가

--- src/Member.java
+++ src/Member.java
@@ -30,4 +30,38 @@ public class Member extends User implements Serializable {
                 ", reservationList=" + reservationNumberList +
                 '}';
     }
+
+    // point 누적
+    public void updatePoints(int additionalPoints) {
+        this.point += additionalPoints; // 포인트 업데이트
+        updateGrade(); // 등급도 업데이트 처리~!
+    }
+
+    // point에 기반한 grade 산정 helper 함수
+    private void updateGrade() {
+        if (this.point >= 10000000) {
+            this.grade = Grade.MVG;
+        } else if (this.point >= 5000000) {
+            this.grade = Grade.VVIP;
+        } else if (this.point >= 1000000) {
+            this.grade = Grade.VIP;
+        } else {
+            this.grade = Grade.BASIC;
+        }
+    }
+
+    // 할인율 적용
+    public double getDiscountRate() {
+        switch (this.grade) {
+            case MVG:
+                return 0.30;
+            case VVIP:
+                return 0.20;
+            case VIP:
+                return 0.10;
+            default:
+                return 0.00;
+        }
+    }
+
 }
